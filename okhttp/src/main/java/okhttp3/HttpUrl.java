8979b3ba7c0acbbb59e7dff952d995fe5753ab6e
jilees <jiles.ru@gmail.com>
2017-10-02 15:33:01 +0300

    Update HttpUrl.java
    Добавил квадратные скобки для перекодировки URL

--- okhttp/src/main/java/okhttp3/HttpUrl.java
+++ okhttp/src/main/java/okhttp3/HttpUrl.java
@@ -290,7 +290,7 @@ public final class HttpUrl {
       {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
   static final String USERNAME_ENCODE_SET = " \"':;<=>@[]^`{}|/\\?#";
   static final String PASSWORD_ENCODE_SET = " \"':;<=>@[]^`{}|/\\?#";
-  static final String PATH_SEGMENT_ENCODE_SET = " \"<>^`{}|/\\?#";
+  static final String PATH_SEGMENT_ENCODE_SET = " \"<>^`{}|/\\?#[]";
   static final String PATH_SEGMENT_ENCODE_SET_URI = "[]";
   static final String QUERY_ENCODE_SET = " \"'<>#";
   static final String QUERY_COMPONENT_ENCODE_SET = " \"'<>#&=";
